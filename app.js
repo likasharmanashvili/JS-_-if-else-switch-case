f569d6f58968f57b8b64b6307ed532970efdd316
lika <l.sharmanashvili@ssu.edu.g>
2024-01-18 14:04:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+//if else function
+// logikuri piroba tu es if... sxva shemtxvevashi else
+// if( 2 > 1 && 6 >5){
+//     console.log ("daregistrirebuli xar")
+// }
+//  else {
+//     console.log("ar xar daregistrirebuli")
+// }
+//piroba shesrulda pirveli da daloga shesabamisad pirveli 
+//if mxolod 1 shegvidzlia davwerot if else bevri elseic erti
+//else - default
+
+// let input = "lika123"
+// if(input === "") { 
+// console.log("pls write something")
+// }
+// else{
+//     console.log("got it")
+// }
+
+
+// let input = "lika1234567"
+
+// if(input.length < 20){
+//     console.log("you're registered");
+// }
+// else if (input.length < 10){
+//     console.log("it should be more than 10")
+// }
+// else if(input.length > 20) {
+//  console.log("error")
+// }
+// else{ 
+//     console.log("error")
+// }
+  
+
+
+
+// gavigot rashemtxvevashi varegistrirebt users
+
+// let firstname = "lika"
+// let lastname ="sharmanashvili"
+// let email = "lsharm@gmail.com"
+ 
+// if (firstname !== "" || lastname !== "" || email !== ""){
+//     console.log("Wellcome")
+// } 
+// //eseigi tu fisrname da ase shemdge ar aris carieli mashin wellcome sxva shentxvevashi user not found
+// else {
+//     console.log("user not found")
+// }
+
+//prompt roca consolelogis dros gamodis ragac fanjara da vwert ragac konkretul informacias
+// prompt("enter your name")
+ 
+// let input = prompt ("enter something....")
+// console.log (input);
+
+
+// even odd logika romlis mixedviiac gavigebt ricxvi aris luwi tu kenti
+// let input = prompt ("enter something")
+//  if(input % 2 === 0){
+//     console.log (input)
+//  }
+//  else {
+//     console.log("this number is odd")
+//  }
+//promptshi dalogas an inputs and kidev this is odd
+
+//switch case
+//switshi shi vwert im logikas romelsac vkoft mere casebad
+//case ukurebs swicth 
+//break nishnavs tu  magis zemot case shesrulda mashin gacherdi
+//tu breaks ar vwert mashin tu orive piroba sruldeba orive ertad shesruldeba
+//case shegvidzlia bevrejer davwerot
+// bolos aris defaulti iigive rogorc if elseshi 
+//elseia iseve tu arcerti piroba ar sruldeba eshveba defualti
+// switch (key)  {
+//     case value: 
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// amocanebi 
+//
+
